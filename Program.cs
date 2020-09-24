a5ba00b7a0882717fb8102bc1c147bd1e9d69cb5
Marce1298 <marchebordon@gmail.com>
2020-09-24 00:43:54 -0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BucleWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("¿Deseas entran en el bucle while?");
+
+            string respuesta = Console.ReadLine();
+
+            while (respuesta != "no")
+            {
+                Console.WriteLine("Estas ejecuanto el interior del bucle while");
+
+                Console.WriteLine("Introduce tu nombre porfavor");
+
+                string nombre = Console.ReadLine();
+
+                Console.WriteLine($"Saldrás del bucle {nombre} cuando respondas 'no'a la pregunta");
+
+                Console.WriteLine("¿Deseas repetir otra vez?");
+
+                respuesta = Console.ReadLine();
+            }
+            Console.WriteLine("Has salido del bucle");
+
+
+        }
+    }
+}
